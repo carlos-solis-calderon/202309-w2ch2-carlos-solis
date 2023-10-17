a3ae109ff69ef700d5608bdb6060c0b533c4e200
Carlos Solis <mrsoliscalderon@gmail.com>
2023-10-18 00:24:34 +0200

    Add scripts for Javascript exercise

---
+++ scripts/tools.js
@@ -0,0 +1,21 @@
+export const strictEquals = (a, b) => {
+  if (isNaN(a) && isNaN(b)) {
+    return false;
+  }
+
+  if (Object.is(a, 0) && Object.is(b, -0)) {
+    return true;
+  }
+
+  if (Object.is(a, -0) && Object.is(b, 0)) {
+    return true;
+  }
+
+  if (Object.is(a, b)) {
+    return true;
+  }
+
+  if (!Object.is(a, b)) {
+    return false;
+  }
+};
